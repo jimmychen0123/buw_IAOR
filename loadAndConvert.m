d43d588e933d2f37ce8be1b4053cc44925f8a5f5
Martin Unterreiner <martin.unterreiner@posteo.de>
2021-04-25 10:49:57 +0200

    Split up loadAndConvert and loadImage

--- loadAndConvert.m
+++ loadAndConvert.m
@@ -1,13 +1,17 @@
-function grayScaleImage = loadAndConvert (fileName)
+function [gray_image] = loadAndConvert (fileName)
 
   %loads and converts the image to a grayscaled image
   disp(fileName);
   originImage = imread(fileName);
     
-  grayScaleImage = uint8(mean(rgb2gray(originImage), 3));
+  % rgb2 and calculate the mean is redundant. Either rgb2gray or the main  
+  %grayScaleImage = uint8(mean(rgb2gray(originImage), 3));
+  %gray_image = uint8(mean(originImage), 3);
   
-  figure(1), imshow(grayScaleImage);
-  figure(2), imhist(grayScaleImage);
+  gray_image = uint8(rgb2gray(originImage));
+  
+  %figure(1), imshow(grayScaleImage);
+  %figure(2), imhist(grayScaleImage);
   
   #{
   Shortly describe the characteristics of the histogram: 
