df9065a5fa4dbbcac32e798a2dd7d81a58758279
Martin Unterreiner <martin.unterreiner@posteo.de>
2021-04-25 10:47:58 +0200

    Split up binary in binaryCompareGrayTresh and binary

--- binary.m
+++ binary.m
@@ -1,45 +1,8 @@
-
-
-function binaryImage = binary (enhancedImage)
+function [binaryImage] = binary(enhancedImage)
   
   % Convert the enhanced image to a binary mask, 
   %   0 = background, 1 = region of interest
+ 
+  binaryImage = im2bw(enhancedImage, graythresh(enhancedImage));
 
-  grayThreshOption = {'moments'; 'mean'; 'Otsu'};
-
-  for i = 1 : size(grayThreshOption, 1)
-    
-    binaryImage = im2bw(enhancedImage, graythresh(enhancedImage, grayThreshOption{i,1}));
-    figure();
-    imshow(binaryImage);
-    
-  endfor
-  
-  n = input('Enter the figure number for morphological operation: ')
-  
-  switch n
-    case 5
-      binaryImage = im2bw(enhancedImage, graythresh(enhancedImage, grayThreshOption{1,1}));
-      saveImage(binaryImage, grayThreshOption{1,1});
-    case 6
-      binaryImage = im2bw(enhancedImage, graythresh(enhancedImage, grayThreshOption{2,1}));
-      saveImage(binaryImage, grayThreshOption{2,1});
-    case 7
-      binaryImage = im2bw(enhancedImage, graythresh(enhancedImage, grayThreshOption{3,1}));
-      saveImage(binaryImage, grayThreshOption{3,1});
-    otherwise
-      binaryImage = im2bw(enhancedImage, graythresh(enhancedImage, grayThreshOption{3,1}));
-      saveImage(binaryImage, grayThreshOption{3,1});
-  endswitch
-##  binaryImage = im2bw(enhancedImage, graythresh(enhancedImage, 'moments'));
-##  figure(5);
-##  imshow(binaryImage);
-##  
-##  binaryImage = im2bw(enhancedImage, graythresh(enhancedImage));
-##  figure(6);
-##  imshow(binaryImage);
-##  
-##  binaryImage = im2bw(enhancedImage, graythresh(enhancedImage, 'mean'));
-##  figure(7);
-##  imshow(binaryImage);
-endfunction
+end
