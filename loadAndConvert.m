54a2d27fd0a7084bcb5718d413bd955bd4935663
Jimmy Chen <jimmychen0123@gmail.com>
2021-04-25 22:43:39 +0200

    add second image for comparison

--- loadAndConvert.m
+++ loadAndConvert.m
@@ -3,19 +3,13 @@ function [gray_image] = loadAndConvert (fileName)
   %loads and converts the image to a grayscaled image
   disp(fileName);
   originImage = imread(fileName);
-    
-  % rgb2 and calculate the mean is redundant. Either rgb2gray or the main  
-  %grayScaleImage = uint8(mean(rgb2gray(originImage), 3));
-  %gray_image = uint8(mean(originImage), 3);
+  figure('name', 'origin image'), imshow(originImage)
   
-  gray_image = uint8(rgb2gray(originImage));
+##  rgb2 and calculate the mean is redundant. Either rgb2gray or the mean
+  gray_image = uint8(mean(originImage, 3));
+  %gray_image = uint8(rgb2gray(originImage));
   
-  %figure(1), imshow(grayScaleImage);
-  %figure(2), imhist(grayScaleImage);
-  
-  #{
-  Shortly describe the characteristics of the histogram: 
-  Histogram is a graph showing the number of pixels in an image(y axis) at each different intensity value(x axis) found in that image.
-  #}
-
+  %figure('name', 'gray image before enhancement'), imshow(gray_image)
+  %figure('name', 'histogram of gray image before enhancement'), imhist(gray_image);
+ 
 endfunction
--- main.m
+++ main.m
@@ -4,24 +4,21 @@ function [outputImage] = main
   % By default installed packages are not available from the Octave prompt. The functions from a package should be added to the Octave path first
   pkg load image;
 
-  %fileName = input('Enter the file name inside single quotes: ');
-
-  grayScaleImage = loadAndConvert('input_sat_image.jpg');
+  %Two images as input
+  gray_image = loadAndConvert('input_sat_image.jpg');
+##  gray_image = loadAndConvert('andreas-gucklhorn-7nV4CZSIoxg-unsplash.jpg');
   
 ##  enhancedImage = enhanceContrast(grayScaleImage);
-  enhancedImage = enhanceContrastV(grayScaleImage);
+  enhancedImage = enhanceContrastV(gray_image);
   
-## To visual compare both images if there are any differences 
+  %To visual compare both images if there are any differences 
 ## figure(1), imshow(enhancedImage);
 ## figure(2), imshow(enhancedImageV);
-##  binaryCompareGraytresh (enhancedImage);
+## binaryCompareGraytresh (enhancedImage);
   
-  binaryImage = binary(enhancedImage);
-  saveImage(~binaryImage, 'binaryImage');
+  binaryImage = binary(enhancedImage);  
+  morphedImage = morphFilter(binaryImage);
   
-  morphedImage = morphFilter(~binaryImage);
+  overlay(enhancedImage, morphedImage);
  
-  outputImage = morphedImage;
-
-  
  end
