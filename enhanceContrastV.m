c15749a58602eee141b22a15a281fade91b26a9d
Martin Unterreiner <martin.unterreiner@posteo.de>
2021-04-25 10:48:31 +0200

    Vectorized enhanceContrast

--- enhanceContrastV.m
+++ enhanceContrastV.m
@@ -1,4 +1,4 @@
-function [enhancedImage] = enhanceContrast (grayImage)
+function [enhancedImage] = enhanceContrastV (grayImage)
   
   %Image enhancement
   % 
@@ -11,19 +11,17 @@ function [enhancedImage] = enhanceContrast (grayImage)
     range = max_value-min_value;
     
     %for every pixel in the image
-    for i = 1:size(grayImage,1)
-        for j = 1:size(grayImage,2)
+    %for i = 1:size(grayImage,1)
+    %    for j = 1:size(grayImage,2)
             %calculate the contrast enhancement 
             % I[r,c] = I[r,c]-min/max-min
-            contrast = double(grayImage(i,j) - min_value)/double(range);
-            enhancedImage(i,j) = uint8(contrast*255);
-        end
-    end
-    %figure(3), imshow(enhancedImage);
-    %figure(4), imhist(enhancedImage);
+            %contrast = double(grayImage(i,j) - min_value)/double(range);
+            %enhancedImage(i,j) = uint8(contrast*255);
+    %    end
+    %end
     
-    
-     
+    contrast = double(grayImage - min_value)/double(range);
+    enhancedImage = uint8(contrast*255); 
   #{
     Shortly describe the differences to the initial histogram:
     The figure 1 shows a low-contrast image as the the values of x axis in intensity hitogram are only distrbuted in a certain range. To improve the visual quality of the image, One technique of image enhancement is contrast stretching on histogram where the image has to use the full intensity range to display the maximum contrast. Contrast stretching takes an image in which the intensity values span the full intensity range as the figure 3 and 4 shown. 
