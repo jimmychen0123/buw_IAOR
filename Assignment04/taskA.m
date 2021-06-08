10645f73c7350aaca403d8e8c51eeb497eb04fed
Martin Unterreiner <martin.unterreiner@posteo.de>
2021-06-08 11:19:36 +0200

    Started with gauss

--- Assignment04/taskA.m
+++ Assignment04/taskA.m
@@ -1,7 +1,7 @@
 function [noise_img] = taskA(gray_image)
     
     noise_img = imnoise(gray_image, 'gaussian', 0, 0.01);
-    figure('name', 'Task A: Imnoise image'), imshow(noise_img);
+    
     %FFT:
     % F(u,v) = 1/M*N SUM(0,M-1)SUM(0,N-1) f(x,y)*e^(j*2*pi*(u*x/M + l*y/N))
     % for every u, v
@@ -13,15 +13,16 @@ function [noise_img] = taskA(gray_image)
     %log
     
     %apply Gauss2D Filter
-    
+    sigma = 0; % Just a dummy variable;
+    gauss_image = gauss2dFilter(noise_img, sigma);
     
 
 end
 
-function gauss2dFilter(noise_img)
-    term_1 = (x./2*pi*sigma^2);
-    term_2 = -(x.^2 + y.^2) / 2*sigma^2;
-    gauss2D = term_1.*exp( term_2 );
+function [gauss2D] = gauss2dFilter(noise_img, sigma)
+    term_1 = (1 / (2*pi*sigma^2));
+    term_2 = (x^2 + y.^2) / (2*sigma^2);
+    gauss2D = term_1.*exp( -term_2 );
     
     % add padding
     
